1664960b44762109d410e14eb97c51643ef3246d
Shash <shashwighton@gmail.com>
2022-07-14 15:34:50 +0400

    This is the first commit

---
+++ utility_cuts.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+def latest_disruption():
+    url = "https://www.gwp.ge/ka/gadaudebeli"
+    response = requests.get(url)
+    text = response.text
+    gwp_data = BeautifulSoup(text, "html.parser")
+
+    #get paths to disruptions
+    disruptions = []
+    for link in gwp_data.find_all("a"):
+        url = link.get("href")
+        if "en/gadaudebeli" in url:
+            disruptions.append(url)
+
+    #disruption is 4th listed (today, need to check regularly to confirm)
+    gwp_url = "https://www.gwp.ge/"
+    disruption_url = gwp_url + disruptions[3]
+    return disruption_url
+
+def disruption_text():
+    url = latest_disruption()
+    response = requests.get(url)
+    text = response.text
+    disruption_data = BeautifulSoup(text, "html.parser")
+    print(disruption_data.get_text())
+
+disruption_text()
+
+
