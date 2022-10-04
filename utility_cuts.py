ec0fd3bf1862295d4126db9b9a948350127b28ae
Shash Wighton <shashwighton@gmail.com>
2022-10-04 11:40:33 +0400

    Updates to code

--- utility_cuts.py
+++ utility_cuts.py
@@ -1,25 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-def latest_disruption():
-    url = "https://www.gwp.ge/ka/gadaudebeli"
+# Takes the URL from a page of disruptions and returns the path to the latest disruption
+def latest_disruption_path(url: str):
     response = requests.get(url)
     text = response.text
-    gwp_data = BeautifulSoup(text, "html.parser")
+    soup = BeautifulSoup(text, "html.parser")
+    path = soup.find("tr").find("a").get("href")
+    
+    return url + path
 
-    #get paths to disruptions
-    disruptions = []
-    for link in gwp_data.find_all("a"):
-        url = link.get("href")
-        if "ka/gadaudebeli" in url:
-            disruptions.append(url)
 
-    #disruption is 4th listed (today, need to check regularly to confirm)
-    gwp_url = "https://www.gwp.ge/"
-    disruption_url = gwp_url + disruptions[3]
-
-    return disruption_url
 
 #A simple function to save the disruptions to a new file
 def save_to_file(disruption_data):
@@ -37,33 +28,49 @@ def disruptions_by_street(disruption_text, streetname: str):
     return disruptions
     
     
-
-def get_disruptions():
-    url = latest_disruption()
+# Returns a list of unplanned water outages
+def get_unplanned__gwp_disruptions():
+    url = latest_disruption_path("https://www.gwp.ge/ka/gadaudebeli")
     response = requests.get(url)
     text = response.text
-    GWP_soup = BeautifulSoup(text, "html.parser")
+    soup = BeautifulSoup(text, "html.parser")
     #extract a PageElement of only the disruptions, which are found in <div class="initial">
-    disruptions_all = GWP_soup.find("div", "initial")
+    disruptions = soup.find("div", "initial").find_all("li")
     #selects only the list tags, each <li> contains a separate disruption:
         #content with <p> tag is the description of the disruption
         #content with <strong> tag contains the streets affected
-    disruptions_all_text = disruptions_all.find_all("li")
 
-    return disruptions_all_text
-    
+    return disruptions
 
+# Returns a list of planned water outages
+def get_planned_gwp_disruptions():
+    url = latest_disruption_path("https://www.gwp.ge/ka/dagegmili")
+    response = requests.get(url)
+    text = response.text
+    soup = BeautifulSoup(text, "html.parser")
+    planned_disruptions = soup.find("div", {"class" : lambda L: L and L.endswith('news-details')}).find_all("p")
+
+    return planned_disruptions[:-3]
 
 
 def main():
+    unplanned_gwp_disruptions = get_unplanned__gwp_disruptions()
+    planned_gwp_disruptions = get_planned_gwp_disruptions()
+    for d in planned_gwp_disruptions:
+        print(d.get_text())
+    print()
+    for d in unplanned_gwp_disruptions:
+        print(d.get_text())
+
+
     # Ask the user for the street to check for disruptions
     streetname = "იყალთო"#input("Streetname: ")
 
     # Get the day's disruptions affecting all streets in Tbilisi
-    all_disruptions = get_disruptions()
+    # all_disruptions = get_disruptions()
 
     # Filter the disruptions by provided streetname
-    filtered_disruptions = disruptions_by_street(all_disruptions, streetname)
+    filtered_disruptions = disruptions_by_street(unplanned_gwp_disruptions, streetname)
 
     if len(filtered_disruptions) == 0:
         print("Huzzah, You may bathe to your heart's content this day")
@@ -96,3 +103,5 @@ if __name__ == "__main__":
 # Turn the program into a bot that runs X times/day and sends you updates
 
 # NOTE - 
+# First line of program check if datetime from header == datetime from last check
+# only run program if not
